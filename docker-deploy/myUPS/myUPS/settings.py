ca44e797891077a528f98a7e67d44f05e318489b
Xushan797 <96745697+Xushan797@users.noreply.github.com>
2022-10-02 10:48:29 -0400

    Update settings.py

--- docker-deploy/myUPS/myUPS/settings.py
+++ docker-deploy/myUPS/myUPS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fo1ccxx8a*t!ry_wb*)4mw7l)4g66@f&3&l9bnmt568#o5c6*0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
